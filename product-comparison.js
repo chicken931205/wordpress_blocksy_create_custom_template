505e4e71169c581670d7bd21f9aba3a75fc54f38
Golden Chicken <chicken931205@outlook.com>
2024-07-16 03:59:15 -0700

    small change

--- product-comparison.js
+++ product-comparison.js
@@ -3,7 +3,6 @@ jQuery(document).ready(function($) {
     var $document = $(document);
     var $whatyoulearn = $('div.what-you-learn');
     var $progressRing = $('.reading-progress svg.progress-circle circle:last-child');
-    var sections = ['benefits', 'key-ingredients', 'top-5'];
 
     var offsetWhatYouLearn = $whatyoulearn.offset();
     
@@ -27,45 +26,10 @@ jQuery(document).ready(function($) {
         $progressRing.css('stroke-dashoffset', offset);
     }
 
-    // function updateActiveSection() {
-    //     var scrollPosition = $window.scrollTop();
-
-    //     sections.forEach(function(section) {
-    //         var $section = $('#' + section);
-    //         if ($section.length) {
-    //             var sectionTop = $section.offset().top - 100;
-    //             var sectionBottom = sectionTop + $section.height();
-
-    //             if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
-    //                 $sidebarNav.find('a').removeClass('active');
-    //                 $sidebarNav.find('a[href="#' + section + '"]').addClass('active');
-    //             }
-    //         }
-    //     });
-    // }
-
     $window.on('scroll', function() {
         updateProgress();
-        // updateActiveSection();
     });
-
-    // $nav.find('a').on('click', function(e) {
-    //     e.preventDefault();
-    //     var target = $(this).attr('href');
-    //     $('html, body').animate({
-    //         scrollTop: $(target).offset().top - 60
-    //     }, 500);
-    // });
-
-    // $sidebarNav.find('a').on('click', function(e) {
-    //     e.preventDefault();
-    //     var target = $(this).attr('href');
-    //     $('html, body').animate({
-    //         scrollTop: $(target).offset().top - 60
-    //     }, 500);
-    // });
 
     // Initialize progress and active section on page load
     updateProgress();
-    // updateActiveSection();
 });
